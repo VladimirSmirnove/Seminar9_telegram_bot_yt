3194f79ff7f3812dd10af7bd6ac3ce0add877990
Vladimir <vladsmir323@gmail.com>
2022-08-20 08:07:18 +0300

    Yourtube_Telegram_Bot

---
+++ main.py
@@ -0,0 +1,5 @@
+from yourtube import executor, dp
+from config import *
+
+if __name__ == '__main__':
+    executor.start_polling(dp) 
---
+++ yourtube.py
@@ -0,0 +1,32 @@
+from aiogram import *
+from config import *
+from pytube import YouTube
+import os
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_message(message:types.Message):
+    chat_id = message.chat.id
+    await bot.send_message(chat_id, 'Привет бро, я могу скачивать видео с Youtube\n'
+                                    'Отправь мне ссылку')
+
+@dp.message_handler()
+async def text_message(message:types.Message):
+    chat_id = message.chat.id
+    url = message.text
+    yt = YouTube(url)
+    if message.text.startswith == 'https://www.youtu.be/' or 'https://www.youtube.com/':
+        await bot.send_message(chat_id, f'Начинаю загрузку видео : {yt.title}\n'
+                                        f'С канала : [{yt.author}]({yt.channel_url})', parse_mode="Markdown")
+        await download_youtube_video(url, message, bot)
+ 
+async def download_youtube_video(url, message, bot):
+    yt = YouTube(url)
+    stream = yt.streams.filter(progressive=True,file_extension='mp4')
+    stream.get_highest_resolution().download(f'{message.chat.id}', f'{message.chat.id}_{yt.title}')
+    with open(f'{message.chat.id}/{message.chat.id}_{yt.title}', 'rb') as video:
+        await bot.send_video(message.chat.id, video, caption='Вот ваше видео', parse_mode='Markdown')
+        os.remove(f'{message.chat.id}/{message.chat.id}_{yt.title}')
+
